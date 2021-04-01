3c97e0fe79d51ed5fb462c0e09c7eab5a5d2e623
Ludmila <ludmila.nieczy@gmail.com>
2021-04-01 14:34:14 -0300

    initial setup

---
+++ src/styles/globalStyle.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+*{
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+}
+
+html, body{
+width: 100%;
+  min-height: 85vh;
+  scroll-behavior:smooth;
+}
+
+body{
+    background-color:${({ theme }) => theme.colorBg};
+    color:${({ theme }) => theme.gray200};
+    font-family: 'Source Sans Pro', sans-serif;
+    font-weight:400;
+}
+`;
+
+export default GlobalStyle;
---
+++ src/styles/theme/index.js
@@ -0,0 +1,19 @@
+const theme = {
+  colorBg: "#F9F9F9",
+  colorPrimary: "#15DBFF",
+  colorSecondary: "#14CAF6",
+  colorWhite: "#FFFFFF",
+  colorOffwhite: "#F9F9F9",
+  colorSuccess: "#F44336",
+  gray100: "#EDEDED",
+  gray200: "#A3A3A3",
+  gray300: "#616161",
+  mediaQuery:{
+      mediaSm:'(min-width:600px)',
+      mediaMd:'(min-width:960px)',
+      mediaLg:'(min-width:1280px)',
+  }
+
+};
+
+export default theme
