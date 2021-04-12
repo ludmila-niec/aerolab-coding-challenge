64636ac18af26e8651473de3d32b8a1be2a42008
Ludmila <ludmila.nieczy@gmail.com>
2021-04-12 13:26:50 -0300

    added Footer component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Home from "./screens/Home";
 import History from "./screens/History";
 import { Switch, Route } from "react-router-dom";
@@ -7,12 +8,13 @@ function App() {
   return (
     <>
       <Header />
-      <main>
+      <main style={{ minHeight: "90vh" }}>
         <Switch>
           <Route path="/home" component={Home} />
           <Route path="/history" component={History} />
         </Switch>
       </main>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/styled.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  padding: 2rem;
+  background-color: #ffffff;
+
+  @media ${({theme}) => theme.mediaQuery.mediaSm}{
+      flex-direction:row;
+  }
+  @media ${({theme}) => theme.mediaQuery.mediaLg}{
+    padding:1rem;
+  }
+`;
+
+export const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  &.author {
+    margin-bottom: 2rem;
+  }
+
+  @media ${({theme}) => theme.mediaQuery.mediaSm}{
+      &.author{
+        margin-bottom:0;
+      }
+  }
+
+`;
+
